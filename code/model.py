da147245c96a0cbcbe706d9f384ff7eedcf687bd
mRSun15 <3150105645@zju.edu.cn>
2018-07-18 23:27:46 +0800

    fix model error, expected stride error modified model.py

--- code/model.py
+++ code/model.py
@@ -41,7 +41,7 @@ class GANModel(nn.Module):
         self.domain_classifier.add_module('d_softmax', nn.LogSoftmax())
 
     def forward(self, input_data, alpha):
-        input_data = input_data.expand(input_data.data.shape[0], self.max_length, self.input_dim)
+        input_data = input_data.expand(input_data.data.shape[0],1,self.max_length, self.input_dim)
         feature = self.feature(input_data)
         feature = feature.view(-1, 50 * 5 * 5)
         reverse_feature = ReverseLayerF.apply(feature, alpha)
