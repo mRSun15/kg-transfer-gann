5de4b6df4062c6a0cdf681fd86e2c219f3f059a9
mRSun15 <3150105645@zju.edu.cn>
2018-07-22 11:06:22 +0800

    add removing_NA.py removing_NA relation

--- code/removing_NA.py
+++ code/removing_NA.py
@@ -6,8 +6,8 @@ wiki_held_out_data = np.load('../data/wiki_held-out_data.npy')
 wiki_held_out_label = np.load('../data/wiki_held-out_label.npy')
 wiki_validation_data = np.load('../data/wiki_validation_data.npy')
 wiki_validation_label = np.load('../data/wiki_validation_label.npy')
-nyt_train_data = np.load('../data/nyt_train_data.npy')
-nyt_train_label = np.load('../data/nyt_train_label.npy')
+# nyt_train_data = np.load('../data/nyt_train_data.npy')
+# nyt_train_label = np.load('../data/nyt_train_label.npy')
 nyt_test_data = np.load('../data/nyt_test_data.npy')
 nyt_test_label = np.load('../data/nyt_test_label.npy')
 
@@ -20,8 +20,8 @@ new_wiki_held_out_data = wiki_held_out_data[wiki_held_out_label != relation_id]
 new_wiki_held_out_label = wiki_held_out_label[wiki_held_out_label != relation_id]
 new_wiki_validation_data = wiki_validation_data[wiki_validation_label != relation_id]
 new_wiki_validation_label = wiki_validation_label[wiki_validation_label != relation_id]
-new_nyt_train_data = nyt_train_data[nyt_train_label != relation_id]
-new_nyt_train_label = nyt_train_label[nyt_train_label != relation_id]
+# new_nyt_train_data = nyt_train_data[nyt_train_label != relation_id]
+# new_nyt_train_label = nyt_train_label[nyt_train_label != relation_id]
 new_nyt_test_data = nyt_train_data[nyt_test_label != relation_id]
 new_nyt_test_label = nyt_train_label[nyt_test_label != relation_id]
 
@@ -32,7 +32,7 @@ np.save('../data/wiki_held_data_.npy', new_wiki_held_out_data)
 np.save('../data/wiki_held_label_.npy', new_wiki_held_out_label)
 np.save('../data/wiki_validat_data_.npy', new_wiki_validation_data)
 np.save('../data/wiki_validat_label_.npy', new_wiki_validation_label)
-np.save('../data/nyt_train_data_.npy', new_nyt_train_data)
-np.save('../data/nyt_train_label_.npy', new_nyt_train_label)
+# np.save('../data/nyt_train_data_.npy', new_nyt_train_data)
+# np.save('../data/nyt_train_label_.npy', new_nyt_train_label)
 np.save('../data/nyt_test_data_.npy', new_nyt_test_data)
 np.save('../data/nyt_test_label_.npy', new_nyt_test_label)
