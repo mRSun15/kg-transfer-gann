80b5176ebb054fc5b9381a76e0eb924f4924d95f
mRSun15 <3150105645@zju.edu.cn>
2018-07-22 15:53:26 +0800

    adjust the relation_Id, change the old id 8--->new id 3 fix the  device-side assert error(in cuda) add redirect_relation_id.py

---
+++ code/redirect_relation_id.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+new_wiki_training_label = np.load('../data/wiki_train_label_.npy')
+new_wiki_held_out_label = np.load('../data/wiki_held_label_.npy')
+new_wiki_validation_label = np.load('../data/wiki_validat_label_.npy')
+new_nyt_train_label = np.save('../data/nyt_train_label_.npy')
+new_nyt_test_label = np.load('../data/nyt_test_label_.npy')
+
+new_wiki_validation_label[new_wiki_validation_label == 8] = 3
+new_wiki_held_out_label[new_wiki_held_out_label == 8] = 3
+new_wiki_training_label[new_wiki_training_label == 8] = 3
+new_nyt_test_label[new_nyt_test_label == 8] = 3
+new_nyt_train_label[new_nyt_train_label == 8] = 3
