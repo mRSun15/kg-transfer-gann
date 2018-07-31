30788910c110383cea46178ca2ee84222db1c046
mRSun15 <3150105645@zju.edu.cn>
2018-07-31 11:28:56 +0800

    involve PCNN

--- code/removing_NA.py
+++ code/removing_NA.py
@@ -8,9 +8,10 @@ wiki_validation_data = np.load('../data/wiki_validation_data.npy')
 wiki_validation_label = np.load('../data/wiki_validation_label.npy')
 nyt_train_data = np.load('../data/nyt_train_data.npy')
 nyt_train_label = np.load('../data/nyt_train_label.npy')
+nyt_train_mask = np.load('../data/nyt_train_mask.npy')
 nyt_test_data = np.load('../data/nyt_test_data.npy')
 nyt_test_label = np.load('../data/nyt_test_label.npy')
-
+nyt_test_mask = np.load('../data/nyt_test_mask.npy')
 
 #remove the NA relation, NA's id is 3
 relation_id = 3
@@ -22,9 +23,10 @@ new_wiki_validation_data = wiki_validation_data[wiki_validation_label != relatio
 new_wiki_validation_label = wiki_validation_label[wiki_validation_label != relation_id]
 new_nyt_train_data = nyt_train_data[nyt_train_label != relation_id]
 new_nyt_train_label = nyt_train_label[nyt_train_label != relation_id]
+new_nyt_train_mask = nyt_train_mask[nyt_train_label != relation_id]
 new_nyt_test_data = nyt_test_data[nyt_test_label != relation_id]
 new_nyt_test_label = nyt_test_label[nyt_test_label != relation_id]
-
+new_nyt_test_mask = nyt_test_mask[nyt_test_label != relation_id]
 #save
 np.save('../data/wiki_train_data_.npy', new_wiki_training_data)
 np.save('../data/wiki_train_label_.npy', new_wiki_training_label)
@@ -34,5 +36,7 @@ np.save('../data/wiki_validat_data_.npy', new_wiki_validation_data)
 np.save('../data/wiki_validat_label_.npy', new_wiki_validation_label)
 np.save('../data/nyt_train_data_.npy', new_nyt_train_data)
 np.save('../data/nyt_train_label_.npy', new_nyt_train_label)
+np.save('../data/nyt_train_mask_.npy', new_nyt_train_mask)
 np.save('../data/nyt_test_data_.npy', new_nyt_test_data)
 np.save('../data/nyt_test_label_.npy', new_nyt_test_label)
+np.save('../data/nyt_test_mask_.npy', new_nyt_test_mask)
